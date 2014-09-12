2a3ebcc614b4f8e653bf5c831bfe62a96daa4849
dwicke <drewwicke@gmail.com>
2014-09-12 11:31:28 -0400

    changed bounties to use mrt

--- Bounties/src/sim/app/bounties/Bounties.java
+++ Bounties/src/sim/app/bounties/Bounties.java
@@ -329,7 +329,7 @@ public class Bounties extends SimState {
         for (int x = 0; x < numRobots; x++) {
             //GreedyBot bot = new GreedyBot();
 
-            DyingTableRobot bot = new DyingTableRobot();            
+            MeanTableRobot bot = new MeanTableRobot();            
             robots[x] = bot;
             bot.setId(x);
             //int xloc = random.nextInt(GRID_WIDTH);
