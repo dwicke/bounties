137e98eb1ac63f331d991716c04cda2201e34b36
dwicke <drewwicke@gmail.com>
2014-11-14 11:23:12 -0500

    pushed the priority and clearing time to the standard auctioneer and made them optional

--- Bounties/src/sim/app/bounties/Auctioneer.java
+++ Bounties/src/sim/app/bounties/Auctioneer.java
@@ -5,6 +5,8 @@
  */
 package sim.app.bounties;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import sim.app.bounties.jumpship.Jumpship;
 import sim.engine.SimState;
 import sim.util.Bag;
@@ -15,24 +17,49 @@ import sim.util.Bag;
  */
 public class Auctioneer extends Bondsman {
 
+    int clearingTimes[];
+    int clearTime;
+    boolean isPriority = false;
+    
     Auctioneer(int numGoals, int numTasks, Jumpship js) {
         super(numGoals,numTasks,js);
+        clearingTimes = new int[numTasks];
+        clearTime = 5;
+        Arrays.fill(clearingTimes, clearTime);
     }
+    
+    public void setIsPriority(boolean priority) {
+        isPriority = priority;
+    }
+    
+    public void setClearTime(int clearTime) {
+        this.clearTime = clearTime;
+    }
+
+    @Override
+    public Bag getAvailableTasks() {
+        if (isPriority == true)
+            return prioritizeTasks(super.getAvailableTasks());
+        Bag t = super.getAvailableTasks();
+        t.shuffle(bounties.random);
+        return t;
+    }
+    
     
     
     @Override
     public void step(SimState state) {
-        // for each of the tasks from the available task in a random order
+        // for each of the tasks from the available task in a priority order
         // ask for bids from each of the agents 
         // allocate the task to the agent with the smallest bid (pick randomly if tied for best)
         this.makeAvailable();// reset the tasks when done and ready
         incrementBounty();// increment the bounties
         Bag availTasks = getAvailableTasks();
-        availTasks.shuffle(state.random);
         Bag availAgents = getAvailableAgents();// only have to do this once and then remove the winner
         for (int i = 0; i < availTasks.numObjs; i++) {
             Bag topBidders = new Bag();
             Task curTaskAuction = (Task) availTasks.objs[i];
+            clearingTimes[curTaskAuction.getID()] = clearingTimes[curTaskAuction.getID()] - 1;
             double smallestBid = Double.MAX_VALUE;
             if(availAgents.numObjs == 0) {
                 System.err.println("NO AGENTS");
@@ -55,10 +82,14 @@ public class Auctioneer extends Bondsman {
             System.out.println("availTasks:" + availAgents.numObjs + " avail tasks:" + availTasks.numObjs);
             //Console.WriteLine("");
             // pick the winner randomly if ties
-            int winner = state.random.nextInt(topBidders.numObjs);
-            ((AuctionAgent)(topBidders.objs[winner])).setTask(curTaskAuction);
-            curTaskAuction.setAvailable(false);// make sure it is set to false
-            availAgents.remove(topBidders.objs[winner]);// remove the winner from avail agents
+            
+            if(clearingTimes[curTaskAuction.getID()] == 0) {
+                clearingTimes[curTaskAuction.getID()] = clearTime;
+                int winner = state.random.nextInt(topBidders.numObjs);
+                ((AuctionAgent)(topBidders.objs[winner])).setTask(curTaskAuction);
+                curTaskAuction.setAvailable(false);// make sure it is set to false
+                availAgents.remove(topBidders.objs[winner]);// remove the winner from avail agents
+            }
         }
         
         
@@ -73,6 +104,18 @@ public class Auctioneer extends Bondsman {
             }
         }
         return availAgents;
+    }
+
+    private Bag prioritizeTasks(Bag availTasks) {
+        
+        availTasks.sort(new Comparator() {
+
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Task) o1).getCurrentReward() - ((Task) o2).getCurrentReward();
+            }
+        });
+        return availTasks;
     }
     
     
