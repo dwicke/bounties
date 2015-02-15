74f4807f62d421de68d61ef7b1e5c36e1ebd26e8
dwicke <drewwicke@gmail.com>
2015-02-15 18:34:22 -0500

    Deleted the robot implementations and created a badValuator

---
+++ Bounties/src/sim/app/bounties/agent/control/BadValuator.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim.app.bounties.agent.control;
+
+import ec.util.MersenneTwisterFast;
+import sim.app.bounties.Bondsman;
+import sim.app.bounties.Task;
+import sim.util.Bag;
+
+/**
+ *
+ * @author drew
+ */
+public class BadValuator extends DefaultValuator{
+
+    Bondsman bondsman;
+    public BadValuator(MersenneTwisterFast random, double epsilonChooseRandomTask, int agentID, Bondsman bondsman) {
+        super(random, epsilonChooseRandomTask, agentID);
+        this.bondsman = bondsman;
+    }
+
+    @Override
+    Task pickTask(Task[] availableTasks) {
+        Task curTask = null;
+        
+        while(curTask == null) {
+            
+            int ind = random.nextInt(availableTasks.length);
+            if  (bondsman.whoseDoingTask(availableTasks[ind]).numObjs == 0){
+                curTask = (Task)availableTasks[ind];
+            }
+            
+           
+        }
+        return curTask;
+    }
+
+    @Override
+    public void learn(Task curTask, double reward, Bag agentsWorking, int numTimeSteps) {
+        // nop
+    }
+    
+}
