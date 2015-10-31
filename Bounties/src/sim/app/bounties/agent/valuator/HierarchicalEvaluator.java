63355002301a20db005691f5c7c36d465e6bd3f9
dwicke <drewwicke@gmail.com>
2015-10-31 15:37:23 -0400

    Working on new evaluator

---
+++ Bounties/src/sim/app/bounties/agent/valuator/HierarchicalEvaluator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim.app.bounties.agent.valuator;
+
+import ec.util.MersenneTwisterFast;
+import sim.app.bounties.environment.Task;
+import sim.util.Bag;
+
+/**
+ *
+ * @author drew
+ */
+public class HierarchicalEvaluator extends DefaultValuator implements DecisionValuator{
+
+    public HierarchicalEvaluator(MersenneTwisterFast random, double epsilonChooseRandomTask, int agentID) {
+        super(random, epsilonChooseRandomTask, agentID);
+    }
+
+    @Override
+    Task pickTask(Task[] availableTasks) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void learn(Task curTask, double reward, Bag agentsWorking, int numTimeSteps) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
